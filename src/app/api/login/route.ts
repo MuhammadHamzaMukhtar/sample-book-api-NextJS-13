e0ac3fd2d18af7a85183db3bdb5ece2ee1ebe949
Muhammad Hamza Mukhtar <rhmza4@gmail.com>
2023-04-29 23:11:03 +0500

    middleware & jwt authentication

--- src/app/api/login/route.ts
+++ src/app/api/login/route.ts
@@ -1,24 +1,62 @@
 import currentDateTime from "@/Helper/currentDate";
-import generateRandomString from "@/Helper/generateRandomString";
+// import generateRandomString from "@/Helper/generateRandomString";
 import query from "@/app/neondb";
 import { NextRequest, NextResponse } from "next/server";
-
-const accessToken = generateRandomString(16);
+import jwt from "jsonwebtoken";
+import { hash } from "bcrypt";
+// import { signIn } from "next-auth/react";
+// const accessToken = generateRandomString(16);
 const currentDate = currentDateTime;
 
-export async function POST(request: NextRequest){
-    const req = await request.json();
-    if(req.username && req.password){
-        await query(`INSERT INTO users(username, password, accessToken, created_at) VALUES('${req.username}', '${req.password}', '${accessToken}', '${currentDate}')`);
-        return NextResponse.json({
-            Message: 'User Added Successfully!',
-            username: req.username,
-            accessToken: accessToken,
-        })  
+export async function POST(request: NextRequest) {
+  const req = await request.json();
+  if (req.username && req.password) {
+    const dbUser = await query(
+      `SELECT * FROM users WHERE username='${req.username}'`
+    );
+    if (dbUser.length > 0) {
+      return new NextResponse("Username already exists!", {
+        status: 403,
+        statusText: "Validation Error!",
+      });
     } else {
-        return NextResponse.json('Missing Email or Password', {
-            status: 403,
-            statusText: 'Validation Error!'
-        })
+      const token = jwt.sign(
+        {
+          username: req.username,
+          password: req.password,
+        },
+        process.env.JWT_SECRET_KEY || "jwtsecretkey",
+        {
+          expiresIn: "2h",
+        }
+      );
+      hash(req.password, 10, async function (err, hash) {
+        if (!err && hash) {
+          await query(
+            `INSERT INTO users(username, password, created_at, accessToken) VALUES('${req.username}', '${hash}', '${currentDate}', '${token}')`
+          );
+        } else {
+          return NextResponse.json(
+            {
+              Message: "Oops! Something went wrong",
+            },
+            {
+              status: 401,
+              statusText: "Validation Error!",
+            }
+          );
+        }
+      });
+      return NextResponse.json({
+        Message: "User Logged In Successfully!",
+        username: req.username,
+        token: token,
+      });
     }
+  } else {
+    return new NextResponse("Missing Email or Password", {
+      status: 403,
+      statusText: "Validation Error!",
+    });
+  }
 }
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,26 +1,53 @@
 // middleware.ts
-import { NextResponse } from 'next/server'
+import { verify } from "jsonwebtoken";
+import { NextRequest, NextResponse } from "next/server";
 
-const allowedOrigins = ['http://localhost:3001', 'https://www.google.com']
+// const allowedOrigins = ["http://localhost:3002", "https://www.google.com"];
 // This function can be marked `async` if using `await` inside
-export function middleware(request: Request) {
-  const origin = request.headers.get('origin');
-  console.log('!allowedOrigins.includes', allowedOrigins.includes('origin'))
-  if(origin && !allowedOrigins.includes('origin')){
-    console.log('origin', origin)
-    return new NextResponse(null, {
-      status: 400,
-      statusText: "Bad Request!",
-      headers: {
-        "Content-type": "text/plain"
+export function middleware(request: NextRequest) {
+  // const origin = request.headers.get("origin");
+  // console.log("!allowedOrigins.includes", allowedOrigins.includes("origin"));
+  // if (origin && !allowedOrigins.includes("origin")) {
+  //   console.log("origin", origin);
+  const accessToken = request.headers
+    .get("authorization")
+    ?.replace("Bearer ", "");
+  if (accessToken) {
+    verify(
+      accessToken,
+      process.env.JWT_SECRET_KEY || "jwtsecret",
+      function (err, decoded) {
+        if (!err && decoded) {
+          return NextResponse.next();
+        } else {
+          return NextResponse.json(
+            {
+              message: "Oops, something went wrong",
+            },
+            {
+              status: 401,
+              statusText: "Unauthorized",
+            }
+          );
+        }
       }
-    })
+    );
   }
-  // return NextResponse.redirect(new URL('/book', request.url))
-  // return NextResponse.json({Error: 'Middleware Applied!'})
+  return NextResponse.json(
+    {
+      message: "Bad Request!",
+    },
+    {
+      status: 401,
+      statusText: "Unauthorized",
+      headers: {
+        "Content-type": "text/plain",
+      },
+    }
+  );
 }
 
 // See "Matching Paths" below to learn more
 export const config = {
-  matcher: '/api/:path*',
-}
+  matcher: "/api/book/:path*",
+};
